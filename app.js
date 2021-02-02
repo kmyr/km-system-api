797363f824c6617177138c3413ebaa1e787bd05b
Kmyr Poureatesam <kmyr.rp@gmail.com>
2021-02-02 11:20:21 +0330

    first commit

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const express = require('express')
-var cors = require('cors')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const app = express()
-require('dotenv/config')
+const http = require("http");
+const WebSocketServer = require("websocket").server;
+const server = http.createServer();
+server.listen(9898, () => {
+  console.log("Server Is Running");
+});
+const wsServer = new WebSocketServer({
+  httpServer: server,
+});
+wsServer.on("request", function (request) {
+  const connection = request.accept(null, request.origin);
+  connection.on("message", function (message) {
+    const recivedMessage = JSON.parse(message.utf8Data)
+   if (recivedMessage.name === "new order") {
+      console.log("Received Message:", message.utf8Data);
+    connection.sendUTF(message.utf8Data);
+   }
 
-app.use(cors())
-app.use(bodyParser.json())
-
-//import routes
-const computerRoute = require('./routes/computers.action')
-const stuffRoute = require('./routes/stuff.action')
-const employeeRoute = require('./routes/employees.action')
-app.use('/computers',computerRoute)
-app.use('/stuff',stuffRoute)
-app.use('/employees',employeeRoute)
-
-
-
-//connect to db
-mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser: true},(error)=> {
-    if (error) {
-        console.log(error);
+    if (message.utf8Data === "kamyar") {
+      console.log("Received Message:", message.utf8Data);
+      connection.sendUTF("Hi this is WebSocket server!");
+      connection.sendUTF("s");
     }
-    console.log('connected to DB!');
-})
-
-
-//start listening to the server
-app.listen(process.env.PORT || 3000);
-
+    // console.log("Received Message:", message.utf8Data);
+    // connection.sendUTF("Hi this is WebSocket server!");
+    // connection.sendUTF("s");
+  });
+  connection.on("close", function (reasonCode, description) {
+    console.log("Client has disconnected.");
+  });
+});
